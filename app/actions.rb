7302a77d08915193dc800acf4a59cab8236e7190
kaizencole <papertigerlily@gmail.com>
2016-06-14 23:19:12 +0000

    Created new_login method

--- app/actions.rb
+++ app/actions.rb
@@ -35,7 +35,7 @@ post '/session' do
     session[:user_session] = SecureRandom.hex
     @user.login_token = session[:user_session]
 
-    if params.has_key?('remember_me') && params[:remember_me] == 'true'
+    if params.has_key?(:remember_me) && params[:remember_me] == 'true'
 
       if @user.remember_token
         response.set_cookie :remember_me, {value: @user.remember_token, max_age: "2592000"}
@@ -87,5 +87,21 @@ post '/messages' do
     redirect '/messages'
   else
     erb :'messages/new'
+  end
+end
+
+get '/new_login' do
+    erb :'new_login'
+end
+
+post '/new_login' do
+  @user = User.new(
+    email: params[:email],
+    password: params[:password_digest]
+  )
+  if @user.save
+    redirect '/login'
+  else
+    erb :'/new_login'
   end
 end
